337d25280269ab48b2db90e19c9e53d295cf57ca
maxwellbenton <maxwellbenton@gmail.com>
2019-01-31 16:02:50 -0500

    adds solution

--- index.js
+++ index.js
@@ -3,5 +3,23 @@ document.addEventListener( 'DOMContentLoaded', () => {
 } )
 
 const getFrontPage = () => {
-  // Add your code here
+  let my_data_fetcher = new XMLHttpRequest();
+  my_data_fetcher.open( 'GET', 'https://api.reddit.com' );
+
+  my_data_fetcher.addEventListener( 'load', e => {
+    JSON.parse( my_data_fetcher.response )
+      .data
+      .children
+      .map( post => {
+        return post
+      } )
+      .slice( 0, 10 )
+      .forEach( post => document.querySelector( 'main' ).insertAdjacentHTML( 'beforeend', `<li><a href=${post.data.url}>${post.data.title}</a></li>` ) );
+  } );
+
+  my_data_fetcher.addEventListener( 'error', ev => {
+    document.querySelector( 'main' ).innerHTML = 'An error has occurred'
+  } );
+
+  my_data_fetcher.send();
 }
